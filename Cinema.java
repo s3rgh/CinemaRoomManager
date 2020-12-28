2fa7c9cf27a566aa2e00ce9321956b7428c1ec3d
Sergey Mezinov <s3rgh@mail.ru>
2020-12-29 02:05:01 +0300

    Stage 3/5: Tickets

--- Cinema.java
+++ Cinema.java
@@ -1,19 +1,78 @@
 package cinema;
 
+import java.util.Scanner;
+
 public class Cinema {
 
     public static void main(String[] args) {
-        // Write your code here
-        System.out.println("Cinema:");
-        System.out.println("  1 2 3 4 5 6 7 8");
-        for (int i = 1; i <= 7; i++) {
-            System.out.print(i + " ");
-            for (int j = 1; j <= 8; j++) {
-                System.out.print("S ");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
+        int rows = scanner.nextInt();
+        System.out.println("Enter the number of seats in each row:");
+        int seats = scanner.nextInt();
+        drawCinema(rows, seats);
+        System.out.println("Enter a row number:");
+        int rowNumber = scanner.nextInt();
+        System.out.println("Enter a seat number in that row:");
+        int seatNumber = scanner.nextInt();
+        printPrice(rowNumber, rows, seats);
+        drawChosenSeat(rowNumber, seatNumber, rows, seats);
+    }
+
+    private static void printPrice(int rowNumber, int rows, int seats) {
+        int allSeats = rows * seats;
+        int price = 0;
+        if (allSeats <= 60) {
+            price = 10;
+        } else {
+            if (rowNumber <= rows / 2) {
+                price = 10;
             }
-            if (i != 7) {
-                System.out.println();
+            if (rowNumber > rows / 2) {
+                price = 8;
             }
         }
+        System.out.println();
+        System.out.println("Ticket price : $" + price);
+        System.out.println();
+    }
+
+    private static void drawChosenSeat(int rowNumber, int seatNumber, int rows, int seats) {
+        System.out.println("Cinema:");
+        System.out.print("  ");
+        for (int i = 1; i <= seats; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 1; i <= rows; i++) {
+            System.out.print(i + " ");
+            for (int j = 1; j <= seats; j++) {
+                if (rowNumber == i && seatNumber == j) {
+                    System.out.print("B ");
+                } else {
+                    System.out.print("S ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    private static void drawCinema(int rows, int seats) {
+        System.out.println();
+        System.out.println("Cinema:");
+        System.out.print("  ");
+        for (int i = 1; i <= seats; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 1; i <= rows; i++) {
+            System.out.print(i + " ");
+            for (int j = 1; j <= seats; j++) {
+                System.out.print("S ");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
